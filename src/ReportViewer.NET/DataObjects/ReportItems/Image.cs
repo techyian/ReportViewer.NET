fd6b7e2535a84cc58778bebdd9f6223f0f836b75
techyian <ian.auty1@outlook.com>
2024-10-16 13:25:56 +0100

    UI changes. Use sticky positioning and use Top/Left positioning.

--- src/ReportViewer.NET/DataObjects/ReportItems/Image.cs
+++ src/ReportViewer.NET/DataObjects/ReportItems/Image.cs
@@ -1,5 +1,4 @@
-﻿using ReportViewer.NET.Parsers;
-using System;
+﻿using System;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
@@ -17,10 +16,7 @@ namespace ReportViewer.NET.DataObjects.ReportItems
                 
         public Image(XElement image, ReportRDL report, ReportItem parent)
             : base(image, report, parent)
-        {
-            this.Style.Top = "";
-            this.Style.Left = "";
-
+        {            
             this.Source = image.Element(report.Namespace + "Source")?.Value;
             this.Value = image.Element(report.Namespace + "Value")?.Value;
             this.Sizing = image.Element(report.Namespace + "Sizing")?.Value;
@@ -36,44 +32,9 @@ namespace ReportViewer.NET.DataObjects.ReportItems
 
             // TODO: Handle sizings?            
             var sb = new StringBuilder();
-            var isLastItem = false;
-                
-            for (var i = 0; i < this.ReportRow.RowItems.Count(); i++)
-            {
-                if (object.ReferenceEquals(this.ReportRow.RowItems[i], this) && i == this.ReportRow.RowItems.Count() - 1)
-                {
-                    isLastItem = true;
-                }
-            }
-                                
-            // Making some decisions here as to the positioning of images. This library is not using absolute positioning
-            // on elements, and images in particular fall foul to this depending on where they are positioned. Making the decision
-            // to left/center/right align based on the width of the row and the left property. If the left property is <= 25% of the width
-            // of the row then left align, <= 50% center align, <= 75 right align.
-            var align = "justify-content: start;";
-            var div = this.ReportRow.RowWidth / 3;
-
-            if (this.Left > div && this.Left <= div * 2)
-            {
-                align = "justify-content: middle;";
-            }
-
-            if (this.Left >= div * 3)
-            {
-                align = "justify-content: end;";
-            }
-                                
+            
             if (!this.Hidden || (this.Hidden && this.Report.ToggleItemRequests.Contains(this.ToggleItem)))
-            {
-                if (isLastItem)
-                {
-                    sb.AppendLine($"<div style=\"display:inline-flex;width:{this.ReportRow.RowWidth - this.Left}mm;{align}\">");
-                }
-                else
-                {
-                    sb.AppendLine($"<div style=\"display:inline-flex;width:auto;{align}\">");
-                }
-
+            {               
                 this.Hidden = false;
                 this.Style.Hidden = false;
 
@@ -85,9 +46,6 @@ namespace ReportViewer.NET.DataObjects.ReportItems
                 {
                     sb.AppendLine(this.BuildDatabaseImage(parent));
                 }
-
-                    
-                sb.AppendLine("</div>");
             }
 
             this.Values = null;
@@ -101,9 +59,9 @@ namespace ReportViewer.NET.DataObjects.ReportItems
             {
                 // TODO: Handle other mime types.
                 case "image/jpeg":
-                    return $"<img class=\"img\" {Style?.Build()} data-toggle=\"{this.ToggleItem}\" src=\"data:image/jpeg;base64, {this.EmbeddedImage.ImageData}\" />";                    
+                    return $"<img class=\"reportviewer-image img\" {Style?.Build()} data-toggle=\"{this.ToggleItem}\" src=\"data:image/jpeg;base64, {this.EmbeddedImage.ImageData}\" />";                    
                 case "image/png":
-                    return $"<img class=\"img\" {Style?.Build()} data-toggle=\"{this.ToggleItem}\" src=\"data:image/png;base64, {this.EmbeddedImage.ImageData}\" />";                    
+                    return $"<img class=\"reportviewer-image img\" {Style?.Build()} data-toggle=\"{this.ToggleItem}\" src=\"data:image/png;base64, {this.EmbeddedImage.ImageData}\" />";                    
             }
 
             return string.Empty;
