21088247c51789ef997fc25cd08b9d1f55ea60af
techyian <ian.auty1@outlook.com>
2024-10-22 06:52:29 +0100

    #17 - Tidy namespaces.

--- src/ReportViewer.NET/Parsers/DateAndTime/MinuteParser.cs
+++ src/ReportViewer.NET/Parsers/DateAndTime/MinuteParser.cs
@@ -2,10 +2,7 @@
 using ReportViewer.NET.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace ReportViewer.NET.Parsers.DateAndTime
 {
