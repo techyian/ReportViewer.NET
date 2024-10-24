66776b9436b229dde6e947b6c7bd3c5e02a6e521
techyian <ian.auty1@outlook.com>
2024-10-24 16:09:04 +0100

    #22 - Unit tests.

---
+++ test/ReportViewer.NET.Tests/Parsers/DateAndTime/TimeSerialParserTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportViewer.NET.DataObjects;
+using ReportViewer.NET.Extensions;
+using ReportViewer.NET.Parsers;
+
+namespace ReportViewer.NET.Tests.Parsers.DateAndTime
+{
+    [TestClass]
+    public class TimeSerialParserTests
+    {
+        private ExpressionParser _expressionParser;
+        private ReportRDL _report;
+
+        public TimeSerialParserTests()
+        {
+            var report = TestHelper.PrimeReport();
+
+            _report = report;
+            _expressionParser = new ExpressionParser(report);
+        }
+
+        [TestMethod]
+        public void TimeSerial_Returns_DateTime()
+        {
+            // Arrange
+            var expr = "=TimeSerial(DatePart(\"h\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday),DatePart(\"n\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday),DatePart(\"s\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday))";
+            
+            // Act
+            var result = _expressionParser.ParseReportExpressionString(
+                expr,
+                null,
+                null,
+                1,
+                _report.DataSets,
+                null,
+                null
+            ).ExpressionAsDateTime();
+
+            // Assert
+            // Allow slight margin.
+            Assert.AreEqual(new DateTime(1, 1, 1, 10, 30, 13), result);
+        }
+
+        [TestMethod]
+        public void TimeSerial_Returns_String()
+        {
+            // Arrange
+            var expr = "=\"TimeSerial parsed as \" & FormatDateTime(TimeSerial(DatePart(\"h\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday),DatePart(\"n\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday),DatePart(\"s\",\"January 15 2010 10:30:13\", FirstDayOfWeek.Monday)), DateFormat.ShortDate)";
+
+            // Act
+            var result = _expressionParser.ParseReportExpressionString(
+                expr,
+                null,
+                null,
+                1,
+                _report.DataSets,
+                null,
+                null
+            ).ExpressionAsString();
+
+            // Assert
+            // Allow slight margin.
+            Assert.AreEqual("TimeSerial parsed as 01/01/0001", result);
+        }
+    }
+}
