2a43f4dfaf92f1ccaffc1b9b6a8927453a49ee7c
techyian <ian.auty1@outlook.com>
2024-10-24 08:16:51 +0100

    #18 - Unit tests.

---
+++ test/ReportViewer.NET.Tests/Parsers/DateAndTime/MonthParserTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportViewer.NET.DataObjects;
+using ReportViewer.NET.Extensions;
+using ReportViewer.NET.Parsers;
+
+namespace ReportViewer.NET.Tests.Parsers.DateAndTime
+{
+    [TestClass]
+    public class MonthParserTests
+    {
+        private ExpressionParser _expressionParser;
+        private ReportRDL _report;
+
+        public MonthParserTests()
+        {
+            var report = TestHelper.PrimeReport();
+
+            _report = report;
+            _expressionParser = new ExpressionParser(report);
+        }
+
+        [TestMethod]
+        public void Month_Returns_Integer()
+        {
+            // Arrange
+            var expr = "=Month(\"January 15, 2010 14:15:30\")";
+
+            // Act
+            var result = _expressionParser.ParseReportExpressionString(
+                expr,
+                null,
+                null,
+                1,
+                _report.DataSets,
+                null,
+                null
+            ).ExpressionAsInt();
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Month_Returns_Integer_Nested_DateAdd()
+        {
+            // Arrange
+            var expr = "=Month(DateAdd(\"m\", 3, \"January 15, 2010 14:15:30\"))";
+
+            // Act
+            var result = _expressionParser.ParseReportExpressionString(
+                expr,
+                null,
+                null,
+                1,
+                _report.DataSets,
+                null,
+                null
+            ).ExpressionAsInt();
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+    }
+}
