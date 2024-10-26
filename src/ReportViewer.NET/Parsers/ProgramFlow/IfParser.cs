5caa6ede8a2e23f43505c3ca2deef72859da8f35
techyian <ian.auty1@outlook.com>
2024-10-26 13:53:57 +0100

    Re-work IfParser to use ParseParenthesis. Unit test.

--- src/ReportViewer.NET/Parsers/ProgramFlow/IfParser.cs
+++ src/ReportViewer.NET/Parsers/ProgramFlow/IfParser.cs
@@ -1,8 +1,6 @@
 ﻿using ReportViewer.NET.DataObjects;
-using ReportViewer.NET.DataObjects.ReportItems;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace ReportViewer.NET.Parsers.ProgramFlow
@@ -33,62 +31,22 @@ namespace ReportViewer.NET.Parsers.ProgramFlow
         public override void Parse()
         {
             // 1. Extract boolean expression
-            var match = IfRegex.Match(CurrentString);
+            var match = IfRegex.Match(this.CurrentString);
             var matchValue = match.Value.Replace("\n", "").Replace("\t", "");
 
             // Remove the surrounding IIF including closing brace so we can inspect inner members and see if they too contain program flow expressions. 
             matchValue = matchValue.Substring(4, matchValue.Length - 5);
 
-            var commaMatches = RegexCommon.CommaNotInParenRegex.Matches(matchValue);
-            //var textInQuotesMatches = TextInQuotesRegex.Matches(matchValue);
-            var indexes = new List<int>();
+            var foundParameters = this.ParseParenthesis(matchValue);
 
-            if (commaMatches.Count == 0)
+            if (foundParameters.Item2.Count != 3)
             {
-                // This has gone wrong.
+                // The IIF function expects 3 parameters.
                 return;
             }
-
-            // We want to end up with 2 commas resulting in 3 groups. If/then/else.
-            if (commaMatches.Count == 2)
-            {
-                // Great, we've found what we're looking for straight away.
-                indexes.AddRange(commaMatches.Select(m => m.Index));
-            }
-            else
-            {
-                // We've got more than we were looking for. So we must now look at commas found within quotes and strip out the ones we don't want.
-                // This probably isn't the most performant way of doing this...
-                foreach (Match commaMatch in commaMatches)
-                {
-                    if (!ExpressionParser.WithinStringLiteral(matchValue, commaMatch.Index))
-                    {
-                        indexes.Add(commaMatch.Index);
-                    }
-                }
-            }
-
-            // We should now have our 2 matches. If not, something has gone wrong.
-            if (indexes.Count != 2)
-            {
-                return;
-            }
-
-            // Let's split our string into its relevant groups.
-            var stringGroups = new List<string>();
-            var removed = 0;
-
-            foreach (var index in indexes)
-            {
-                stringGroups.Add(matchValue.Substring(removed, index - removed));
-                removed += matchValue.Substring(removed, index - removed).Length + 1;
-            }
-
-            // Then grab the last of the string.
-            stringGroups.Add(matchValue.Substring(removed, matchValue.Length - removed));
 
             var booleanExpression = this.Report.Parser.ParseReportExpressionString(
-                stringGroups[0], 
+                foundParameters.Item2[0], 
                 this.DataSetResults, 
                 this.Values, 
                 this.CurrentRowNumber, 
@@ -98,7 +56,7 @@ namespace ReportViewer.NET.Parsers.ProgramFlow
             );
             
             var thenExpression = this.Report.Parser.ParseReportExpressionString(
-                stringGroups[1], 
+                foundParameters.Item2[1], 
                 this.DataSetResults, 
                 this.Values, 
                 this.CurrentRowNumber, 
@@ -108,7 +66,7 @@ namespace ReportViewer.NET.Parsers.ProgramFlow
             );
             
             var elseExpression = this.Report.Parser.ParseReportExpressionString(
-                stringGroups[2], 
+                foundParameters.Item2[2], 
                 this.DataSetResults, 
                 this.Values, 
                 this.CurrentRowNumber, 
---
+++ test/ReportViewer.NET.Tests/Parsers/ProgramFlow/IfParserTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportViewer.NET.DataObjects;
+using ReportViewer.NET.Extensions;
+using ReportViewer.NET.Parsers;
+
+namespace ReportViewer.NET.Tests.Parsers.ProgramFlow
+{
+    [TestClass]
+    public class IfParserTests
+    {
+        private ExpressionParser _expressionParser;
+        private ReportRDL _report;
+
+        public IfParserTests()
+        {
+            var report = TestHelper.PrimeReport();
+
+            _report = report;
+            _expressionParser = new ExpressionParser(report);
+        }
+
+        [TestMethod]
+        public void If_Value_Time_Of_Day_Nested()
+        {
+            // Arrange
+            var expr = "=IIf(Hour(Now()) >= 12 And Hour(Now()) < 18,\"Afternoon\", IIf(Hour(Now()) > 5 And Hour(Now()) < 12, \"Morning\", \"Evening\"))";
+
+            // Act
+            var result = _expressionParser.ParseReportExpressionString(
+                expr,
+                null,
+                null,
+                1,
+                _report.DataSets,
+                null,
+                null
+            ).ExpressionAsString();
+
+            // Assert
+            var now = DateTime.Now;
+
+            if (now.Hour >= 12 && now.Hour < 18)
+            {
+                Assert.AreEqual("Afternoon", result);
+            }
+            else if (now.Hour > 5 && now.Hour < 12)
+            {
+                Assert.AreEqual("Morning", result);
+            }
+            else 
+            {
+                Assert.AreEqual("Evening", result);
+            }            
+        }
+    }
+}
